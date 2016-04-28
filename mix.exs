8b8828fb432280516cd9037ed00c0bb0a775280d
Dimitris Zorbas <zorbash@skroutz.gr>
2016-04-28 18:33:36 +0300

    Add poison to the mix

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule Dash.Mixfile do
+defmodule Kitto.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :dash,
+    [app: :kitto,
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
@@ -10,25 +10,14 @@ defmodule Dash.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [mod: {Dash, []},
+    [mod: {Kitto, []},
      applications: [:logger, :cowboy, :plug]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:cowboy, "~> 1.0.0"},
-     {:plug, "~> 1.0"}]
+     {:plug, "~> 1.0"},
+     {:poison, "~> 2.0"}]
   end
 end
