60eb5ab47a606d40bac24a352ad5361689d83191
Dimitris Zorbas <zorbash@skroutz.gr>
2016-10-11 19:33:32 +0300

    Have mix kitto.server in :dev env watch assets
    Instead of having the user start webpack-dev-server using another shell
    instance, we have the kitto.server task start it.
    Configuration:
    
    Specify a different asset watcher, or different options to webpack:
    
    ```elixir
    config :kitto, :watcher, grunt: [bin: "relative-path-to-grunt-bin",
                                     opts: ["option1", "option2"]]
    ```

--- lib/mix/tasks/kitto.server.ex
+++ lib/mix/tasks/kitto.server.ex
@@ -1,5 +1,9 @@
 defmodule Mix.Tasks.Kitto.Server do
   use Mix.Task
+  require Logger
+
+  @watchers webpack: [bin: "./node_modules/.bin/webpack-dev-server",
+                      opts: ["--stdin", "--progress"]]
 
   @shortdoc "Starts applications and their servers"
 
@@ -15,14 +19,32 @@ defmodule Mix.Tasks.Kitto.Server do
   The `--no-halt` flag is automatically added.
   """
   def run(args) do
+    if Mix.env == :dev && watch_assets?, do: spawn_link(&start_watcher/0)
+
     Mix.Task.run "run", run_args() ++ args
   end
 
-  defp run_args do
-    if iex_running?, do: [], else: ["--no-halt"]
+  defp start_watcher do
+    validate_watcher
+    System.cmd watcher_bin, watcher[:opts]
   end
 
-  defp iex_running? do
-    Code.ensure_loaded?(IEx) && IEx.started?
+  defp validate_watcher do
+    unless watcher_exists? do
+      Logger.error "Could not start watcher because #{watcher_bin} could not" <>
+                   "be found. Your dashboard server is running, but assets won't" <>
+                   "be compiled."
+
+      exit(:shutdown)
+    end
   end
+
+  defp watch_assets?, do: Application.get_env :kitto, :watch_assets?, true
+  defp watcher_exists?, do: File.exists? watcher_bin
+
+  defp watcher, do: Application.get_env(:kitto, :watcher, @watchers[:webpack])
+  defp watcher_bin, do: watcher[:bin] |> Path.expand
+
+  defp run_args, do: if iex_running?, do: [], else: ["--no-halt"]
+  defp iex_running?, do: Code.ensure_loaded?(IEx) && IEx.started?
 end
