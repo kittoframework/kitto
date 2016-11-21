287850890a795ce98a6d9cfed33d5be11a74b18c
Dimitris Zorbas <zorbash@skroutz.gr>
2016-11-21 15:11:40 +0200

    Add code doc for Kitto.Job.DSL

--- lib/kitto/job/dsl.ex
+++ lib/kitto/job/dsl.ex
@@ -1,4 +1,9 @@
 defmodule Kitto.Job.DSL do
+  @moduledoc """
+  A DSL to define jobs populating the widgets with data.
+  """
+
+  @doc false
   defmacro __using__(_opts) do
     quote do
       import Kitto.Job.DSL
@@ -6,6 +11,30 @@ defmodule Kitto.Job.DSL do
     end
   end
 
+  @doc """
+  Main API to define jobs.
+
+  It accepts an expression representing data retrieval and any transformations
+  required to broadcast events to the widgets.
+
+  ## Examples
+
+      use Kitto.Job.DSL
+
+      job :jenkins, every: :minute do
+        jobs = Jenkins.jobs |> Enum.map(fn (%{"job" => job}) -> %{job: job.status} end)
+
+        broadcast! :jenkins, %{jobs: jobs}
+      end
+
+      job :twitter, do: Twitter.stream("#elixir", &(broadcast!(:twitter, &1))
+
+  ## Options
+    * `:every` - Sets the interval on which the job will be performed. When it's not
+    specified, the job will be called once (suitable for streaming resources).
+
+    * `:first_at` - A timeout after which to perform the job for the first time
+  """
   defmacro job(name, options, contents \\ []) do
     quote do
       Kitto.Job.register unquote(name),
