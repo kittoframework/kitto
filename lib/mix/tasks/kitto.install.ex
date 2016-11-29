d67d2c358688d59c38669f82d828b0b6cd52677e
Jan Stevens <jan@playpass.be>
2016-11-29 21:24:59 +0200

    Add Mix.Tasks.Kitto.Install

---
+++ lib/mix/tasks/kitto.install.ex
@@ -0,0 +1,100 @@
+defmodule Mix.Tasks.Kitto.Install do
+  use Mix.Task
+  @shortdoc "Install community Widget/Job from a Github Gist"
+  @supported_languages ["JavaScript", "SCSS", "Markdown", "Elixir"]
+  @github_url "https://api.github.com/gists/"
+
+  @moduledoc """
+  Installs community Widget/Job from a Github Gist
+
+      mix kitto.install --widget test_widget --gist JanStevens/0209a4a80cee782e5cdbe11a1e9bc393
+      mix kitto.install --gist 0209a4a80cee782e5cdbe11a1e9bc393
+
+  ## Options
+
+    * `--widget` - specifies the widget name that will be used as directory name
+      in the widgets directory. By default we use the js filename as directory
+
+    * `--gist` - The gist to download from, specified as `Username/Gist` or `Gist`
+
+  """
+  def run(args) do
+    {:ok, _started} = Application.ensure_all_started(:httpoison)
+    {opts, _parsed, _} = OptionParser.parse(args, strict: [widget: :string, gist: :string])
+    opts_map = Enum.into(opts, %{})
+    process(opts_map)
+  end
+
+  defp process(%{gist: gist, widget: widget}) do
+    files = gist |> String.split("/")
+      |> build_gist_url
+      |> download_gist
+      |> Map.get(:files)
+      |> Enum.map(&extract_file_properties/1)
+      |> Enum.filter(&supported_file_type?/1)
+
+    widget_dir = widget || find_widget_filename(files)
+    files
+      |> Enum.map(&(determine_file_location(&1, widget_dir)))
+      |> Enum.each(&write_file/1)
+  end
+
+  defp process(%{gist: gist}), do: process(%{gist: gist, widget: nil})
+
+  defp process(_) do
+    Mix.shell.error "Unsupported arguments"
+  end
+
+  defp write_file(file) do
+    Mix.Generator.create_directory(file.path)
+    filename = Path.join([file.path, file.filename])
+    Mix.Generator.create_file(filename, file.content)
+  end
+
+  defp determine_file_location(_file, widget_name) when widget_name == nil do
+    Mix.shell.error "Please specify a widget directory using the --widget flag"
+    Mix.raise "Installation failed"
+  end
+
+  # Elixir files we place in the jobs dir
+  defp determine_file_location(%{language: "Elixir"} = file, _) do
+    Map.put(file, :path, "jobs")
+  end
+
+  # Other files all go into the widget dir
+  defp determine_file_location(file, widget_name) do
+    Map.put(file, :path, Path.join(["widgets", widget_name]))
+  end
+
+  defp find_widget_filename(files) do
+    files
+      |> Enum.filter(fn(file) -> file.language == "JavaScript" end)
+      |> List.first
+      |> extract_widget_dir
+  end
+
+  defp extract_widget_dir(%{filename: filename}) do
+    filename |> String.replace(~r/\.js$/, "")
+  end
+
+  defp extract_widget_dir(nil), do: nil
+
+  defp supported_file_type?(file) do
+    Enum.member?(@supported_languages, file.language)
+  end
+
+  def extract_file_properties({_filename, file}), do: file
+
+  defp download_gist(url), do: url |> HTTPoison.get! |> process_response
+
+  defp build_gist_url(gist_url) when length(gist_url) == 1, do: @github_url <> hd(gist_url)
+  defp build_gist_url([_ | gist_url]), do: build_gist_url(gist_url)
+
+  defp process_response(%HTTPoison.Response{status_code: 200, body: body}), do: body |> Poison.decode!(keys: :atoms)
+  defp process_response(%HTTPoison.Response{status_code: code, body: body}) do
+    decoded_body = body |> Poison.decode!(keys: :atoms)
+    Mix.shell.error "Could not fetch the gist from GitHub: " <>
+                    "#{code}: #{decoded_body.message}"
+    Mix.raise "Installation failed"
+  end
+end
---
+++ test/mix/tasks/kitto.install_test.exs
@@ -0,0 +1,115 @@
+defmodule Mix.Tasks.Kitto.InstallTest do
+  use ExUnit.Case, async: false
+  import Mock
+  import Kitto.FileAssertionHelper
+
+  @job_gist_response %{ files:
+    %{"job.ex" => %{filename: "job.ex", language: "Elixir", content: "job"}}
+  }
+
+  @css_gist_response %{ files:
+    %{"number_job.scss" => %{filename: "number_job.scss", language: "SCSS", content: "style"}}
+  }
+
+  @gist_response %{ files:
+    %{
+      "README.md" => %{filename: "README.md", language: "Markdown", content: "Title"},
+      "number_job.ex" => %{filename: "number_job.ex", language: "Elixir", content: "job"},
+      "number.scss" => %{filename: "number.scss", language: "SCSS", content: "style"},
+      "number.js" => %{filename: "number.js", language: "JavaScript", content: "js"}
+    }
+  }
+
+  setup do
+    Mix.Task.clear
+    :ok
+  end
+
+  test "fails when `--gist` is not provided" do
+    Mix.Tasks.Kitto.Install.run(["--widget", "numbers"])
+
+    assert_received {:mix_shell, :error, ["Unsupported arguments"]}
+  end
+
+  test "fails when the gist is not found" do
+    with_mock HTTPoison, [get!: mock_gist_with(404, %{message: "Not Found"})] do
+
+      assert_raise Mix.Error, fn ->
+        Mix.Tasks.Kitto.Install.run(["--widget", "numbers", "--gist", "0209a4a80cee78"])
+
+        assert called HTTPoison.get!("https://api.github.com/gists/0209a4a80cee78")
+      end
+
+      assert_received {:mix_shell, :error, ["Could not fetch the gist from GitHub: 404: Not Found"]}
+    end
+  end
+
+  test "fails when no widget directory is specified or found" do
+    with_mock HTTPoison, [get!: mock_gist_with(200, @css_gist_response)] do
+
+      assert_raise Mix.Error, fn ->
+        Mix.Tasks.Kitto.Install.run(["--gist", "0209a4a80cee78"])
+
+        assert called HTTPoison.get!("https://api.github.com/gists/0209a4a80cee78")
+      end
+    end
+
+    assert_received {:mix_shell, :error, ["Please specify a widget directory using the --widget flag"]}
+  end
+
+  test "places all the files in the correct locations" do
+    in_tmp "installs widgets and jobs", fn ->
+      with_mock HTTPoison, [get!: mock_gist_with(200, @gist_response)] do
+        Mix.Tasks.Kitto.Install.run(["--gist", "0209a4a80cee78"])
+
+        assert_file "widgets/number/number.js", fn contents ->
+          assert contents =~ "js"
+        end
+
+        assert_file "widgets/number/number.scss", fn contents ->
+          assert contents =~ "style"
+        end
+
+        assert_file "widgets/number/README.md", fn contents ->
+          assert contents =~ "Title"
+        end
+        refute_file "widgets/number/number_job.ex"
+
+        assert_file "jobs/number_job.ex", fn contents ->
+          assert contents =~ "job"
+        end
+      end
+    end
+  end
+
+  test "uses the widget overwrite for the widget directory" do
+    in_tmp "installs widgets and jobs using overwrite", fn ->
+      with_mock HTTPoison, [get!: mock_gist_with(200, @gist_response)] do
+        Mix.Tasks.Kitto.Install.run(["--gist", "0209a4a80cee78", "--widget", "overwrite"])
+
+        assert_file "widgets/overwrite/number.js", fn contents ->
+          assert contents =~ "js"
+        end
+
+        assert_file "widgets/overwrite/number.scss", fn contents ->
+          assert contents =~ "style"
+        end
+
+        assert_file "widgets/overwrite/README.md", fn contents ->
+          assert contents =~ "Title"
+        end
+        refute_file "widgets/overwrite/number_job.ex"
+
+        assert_file "jobs/number_job.ex", fn contents ->
+          assert contents =~ "job"
+        end
+      end
+    end
+  end
+
+  def mock_gist_with(status_code, body) do
+    fn (_url) ->
+      %HTTPoison.Response{status_code: status_code, body: Poison.encode!(body)}
+    end
+  end
+end
