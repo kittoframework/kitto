7eeda801218c9af3c76da8cc51478ff5d592b616
Dimitris Zorbas <zorbash@skroutz.gr>
2016-12-03 21:40:47 +0200

    Add Kitto.Time.mseconds for :second, :minute, :hour, :day

--- test/time_test.exs
+++ test/time_test.exs
@@ -12,16 +12,32 @@ defmodule Kitto.TimeTest do
     assert Kitto.Time.mseconds({2, :days}) == 2 * 24 * 3600 * 1000
   end
 
+  test "#mseconds({1, :day})" do
+    assert Kitto.Time.mseconds({1, :day}) == 1 * 24 * 3600 * 1000
+  end
+
   test "#mseconds({n, :hours})" do
     assert Kitto.Time.mseconds({4, :hours}) == 4 * 3600 * 1000
+  end
+
+  test "#mseconds({1, :hour})" do
+    assert Kitto.Time.mseconds({1, :hour}) == 1 * 3600 * 1000
   end
 
   test "#mseconds({n, :minutes})" do
     assert Kitto.Time.mseconds({5, :minutes}) == 5 * 60 * 1000
   end
 
+  test "#mseconds({1, :minute})" do
+    assert Kitto.Time.mseconds({1, :minutes}) == 1 * 60 * 1000
+  end
+
   test "#mseconds({n, :seconds})" do
     assert Kitto.Time.mseconds({7, :seconds}) == 7 * 1000
+  end
+
+  test "#mseconds({n, :second})" do
+    assert Kitto.Time.mseconds({1, :second}) == 1 * 1000
   end
 
   test "#mseconds({n, :milliseconds})" do
