bd2f0c5d899c46c2d604401cb086b2eae8cf8a8b
mackenza <a.mackenzie@gmail.com>
2016-12-31 12:14:39 -0500

    all fields settable by job

--- installer/templates/new/widgets/text/text.js
+++ installer/templates/new/widgets/text/text.js
@@ -6,10 +6,10 @@ import './text.scss';
 Widget.mount(class Text extends Widget {
   render() {
     return (
-      <div className={this.props.className}>
-        <h1 className="title">{this.props.title}</h1>
+      <div className={this.state.classname || this.props.className}>
+        <h1 className="title">{this.state.title || this.props.title}</h1>
         <h3>{this.state.text || this.props.text}</h3>
-        <p className="more-info">{this.props.moreinfo}</p>
+        <p className="more-info">{this.state.moreinfo || this.props.moreinfo}</p>
       </div>
     );
   }
