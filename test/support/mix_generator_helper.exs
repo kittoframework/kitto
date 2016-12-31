ae95d4620886d917aa2b28e764f398756ab262b1
Dave Long <dlong@cagedata.com>
2016-12-30 20:59:28 -0500

    Adds documentation to the MixGeneratorHelper functions

--- test/support/mix_generator_helper.exs
+++ test/support/mix_generator_helper.exs
@@ -6,25 +6,62 @@ defmodule Kitto.MixGeneratorHelper do
   import Mock
   alias Kitto.Mix.GeneratorMock
 
+  @mocks [create_file: &GeneratorMock.create_file/2,
+          create_directory: &GeneratorMock.create_directory/1]
+
+  @doc """
+  Asserts that a file was created (with Mix.Generator.create_file/2) by running
+  a Mix task.
+
+  Arguments:
+
+    * `file`: The file expected to be created. Can be a regex or string
+    * `task`: Function which runs the task that is expected to create a file
+
+  Usage:
+
+      assert_creates_file ~r/my_job.ex/, fn() ->
+        Mix.Kitto.Tasks.Gen.Job.run(["my_job"])
+      end
+  """
   def assert_creates_file(file, task) do
-    with_mock Mix.Generator, [create_file: &GeneratorMock.create_file/2, create_directory: &GeneratorMock.create_directory/1] do
+    with_mock Mix.Generator, @mocks do
       created_file = task.()
       match? = cond do
         is_binary(file) -> created_file == file
         Regex.regex?(file) -> created_file =~ file
       end
-      assert match?, "Expected #{inspect file} to be created. Created #{created_file} instead."
+      assert match?, """
+      Expected #{inspect file} to be created. Created #{created_file} instead.
+      """
     end
   end
 
+  @doc """
+  Asserts that a directory was created( with Mix.Generator.create_directory/1)
+  by running a Mix task.
+
+  Arguments:
+
+    * `dir`: The directory expected to be created. Can be a regex or string
+    * `task`: Function which runs the task that is expected to create a directory
+
+  Usage:
+
+      assert_creates_directory ~r/my_widget/, fn() ->
+        Mix.Kitto.Tasks.Gen.Widget.run(["my_widget"])
+      end
+  """
   def assert_creates_directory(dir, task) do
-    with_mock Mix.Generator, [create_file: &GeneratorMock.create_file/2, create_directory: &GeneratorMock.create_directory/1] do
+    with_mock Mix.Generator, @mocks do
       created_dir = task.()
       match? = cond do
         is_binary(dir) -> created_dir == dir
         Regex.regex?(dir) -> created_dir =~ dir
       end
-      assert match?, "Expected #{inspect dir} to be created. Created #{created_dir} instead."
+      assert match?, """
+      Expected #{inspect dir} to be created. Created #{created_dir} instead.
+      """
     end
   end
 end
