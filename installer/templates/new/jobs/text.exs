921fd985b760ca0a60ba0bfaa1bbcb9d44967f4f
mackenza <a.mackenzie@gmail.com>
2017-01-01 11:07:50 -0500

    more fields & fixes to text widget

--- installer/templates/new/jobs/text.exs
+++ installer/templates/new/jobs/text.exs
@@ -3,5 +3,10 @@ use Kitto.Job.DSL
 job :text, every: {4, :seconds} do
   phrases = ["This is your shiny new dashboard", "Built on the Kitto Framework"]
 
-  broadcast! :text, %{text: (phrases |> Enum.shuffle |> Enum.take(1))}
+  broadcast! :text, 
+    %{
+      text: (phrases |> Enum.shuffle |> Enum.take(1)),
+      moreinfo: "This is where you can put dynamic text",
+      specialStyle: "widget-text-custom"
+    }
 end
--- installer/templates/new/widgets/text/text.js
+++ installer/templates/new/widgets/text/text.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Widget from '../../assets/javascripts/widget';
+import {updatedAt} from '../../assets/javascripts/helpers';
 
 import './text.scss';
 
 Widget.mount(class Text extends Widget {
   render() {
     return (
-      <div className={this.state.classname || this.props.className}>
+      <div className={this.props.className + " " + this.state.specialStyle}>
         <h1 className="title">{this.state.title || this.props.title}</h1>
         <h3>{this.state.text || this.props.text}</h3>
         <p className="more-info">{this.state.moreinfo || this.props.moreinfo}</p>
+        <p className="updated-at">{updatedAt(this.state.updated_at)}</p>
       </div>
     );
   }
