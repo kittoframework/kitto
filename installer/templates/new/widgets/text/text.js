2b2cd223ee1ab42db171b9c148aa900ba2dc967c
mackenza <a.mackenzie@gmail.com>
2017-01-02 14:37:44 -0500

    fixes from code review

--- installer/templates/new/widgets/text/text.js
+++ installer/templates/new/widgets/text/text.js
@@ -5,13 +5,13 @@ import {updatedAt} from '../../assets/javascripts/helpers';
 import './text.scss';
 
 Widget.mount(class Text extends Widget {
+  status() {
+    if (!this.state.status) { return ""; }    
+    return`status-${this.state.status}`;
+  }
   render() {
-    let status = "";
-    if (this.state.widgetStatus) {
-      status = this.state.widgetStatus;
-    }
     return (
-      <div className={`${this.props.className} ${status}`}>    
+      <div className={`${this.props.className} status-${status}`}>    
         <h1 className="title">{this.state.title || this.props.title}</h1>
         <h3>{this.state.text || this.props.text}</h3>
         <p className="more-info">{this.state.moreinfo || this.props.moreinfo}</p>
