b36eaac4949666f75c5f7814200414b842210e73
mackenza <a.mackenzie@gmail.com>
2017-01-02 16:16:13 -0500

    fixed status passing

--- installer/templates/new/widgets/text/text.js
+++ installer/templates/new/widgets/text/text.js
@@ -7,11 +7,13 @@ import './text.scss';
 Widget.mount(class Text extends Widget {
   status() {
     if (!this.state.status) { return ""; }    
+    
     return`status-${this.state.status}`;
   }
+  
   render() {
     return (
-      <div className={`${this.props.className} status-${status}`}>    
+      <div className={`${this.props.className} ${this.status()}`}>    
         <h1 className="title">{this.state.title || this.props.title}</h1>
         <h3>{this.state.text || this.props.text}</h3>
         <p className="more-info">{this.state.moreinfo || this.props.moreinfo}</p>
