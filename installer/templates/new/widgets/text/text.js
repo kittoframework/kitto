adccafe25ee069e3dad551c2be6ba0dbc2e72060
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2017-01-14 19:04:47 +0200

    Strip trailing whitespace from text widget

--- installer/templates/new/widgets/text/text.js
+++ installer/templates/new/widgets/text/text.js
@@ -6,14 +6,14 @@ import './text.scss';
 
 Widget.mount(class Text extends Widget {
   status() {
-    if (!this.state.status) { return ""; }    
-    
+    if (!this.state.status) { return ""; }
+
     return`status-${this.state.status}`;
   }
-  
+
   render() {
     return (
-      <div className={`${this.props.className} ${this.status()}`}>    
+      <div className={`${this.props.className} ${this.status()}`}>
         <h1 className="title">{this.state.title || this.props.title}</h1>
         <h3>{this.state.text || this.props.text}</h3>
         <p className="more-info">{this.state.moreinfo || this.props.moreinfo}</p>
