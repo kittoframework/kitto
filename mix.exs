3380267b2480f3929400ed78d50f2b4687059a48
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2017-04-01 16:50:19 +0100

    Update version of httpoison to 0.11.1
    httpoison@0.11.1 depends on hackney@1.7.1
    
    Versions 1.6.6, 1.6.4 have been retired from https://hex.pm/packages/hackney
    It's better to depend on the latest stable version.

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Kitto.Mixfile do
      {:plug, "~> 1.3.2"},
      {:poison, "~> 3.0"},
      {:fs, "~> 2.12.0"},
-     {:httpoison, "~> 0.10.0"},
+     {:httpoison, "~> 0.11.1"},
      {:ex_doc, "~> 0.14", only: :dev},
      {:credo, "~> 0.5", only: [:dev, :test]},
      {:mock, "~> 0.2", only: :test},
