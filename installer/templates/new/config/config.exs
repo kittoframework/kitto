9db8300d6c6eb7362dd0c5e5c31b5996dc2ec548
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2017-04-09 00:58:10 +0100

    [installer] Set :otp_app in config/config.exs

--- installer/templates/new/config/config.exs
+++ installer/templates/new/config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 use Mix.Config
 
-config :kitto, root: Path.dirname(__DIR__), port: 4000
+config :kitto, root: Path.dirname(__DIR__), port: 4000, otp_app: :<%= application_name %>
 
 # Use reload_code?: false to disable code reloading in development environment
 # Read More: https://github.com/kittoframework/kitto/wiki/Code-Reloading
