348e6bfcff0b00da890b52a39a9c9ef7a1063a84
Nicholas Rutherford <zgithub@isomorphism.ca>
2017-10-15 14:33:45 -0700

    Create a button to launch into fullscreen mode
    We want the button to show up when the mouse is active, so it has
    to listen to mouse events on the whole body. Seeing how there
    isn't a place for internal react components and because it is
    so simple we just use an click listener instead of react.

--- installer/templates/new/assets/javascripts/application.js
+++ installer/templates/new/assets/javascripts/application.js
@@ -2,7 +2,20 @@ import '../stylesheets/application.scss';
 
 import $ from 'jquery';
 import {Kitto} from 'kitto';
+import fscreen from 'fscreen';
 
 window.jQuery = window.$ = $;
 
 Kitto.start();
+
+var i = null;
+$("body").mousemove(function() {
+    clearTimeout(i);
+    $(".fullscreen-button").addClass("active");
+    i = setTimeout('$(".fullscreen-button").removeClass("active");', 1000);
+})
+
+$(".fullscreen-button").click(function() {
+  var ele = document.getElementById("container");
+  fscreen.requestFullscreen(ele);
+})
