587694fa0cfd1d4281c3e072ec6b7871ffb3457b
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2018-03-24 13:30:53 +0000

    Make static assets path configurable via :assets_path

--- installer/templates/new/config/prod.exs
+++ installer/templates/new/config/prod.exs
@@ -3,4 +3,9 @@ use Mix.Config
 config :kitto, reload_code?: false, watch_assets?: false, serve_assets?: true
 
 # For distillery releases
+# Read more: https://github.com/kittoframework/kitto/wiki/%5BDeployment%5D-Distillery
 # config :kitto, root: :otp_app
+
+# For heroku deployments
+# Read more: https://github.com/kittoframework/kitto/wiki/%5BDeployment%5D-Heroku
+# config :kitto, assets_path: "priv/static"
